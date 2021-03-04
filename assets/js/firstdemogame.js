151f6309a7f883a8eeee6a994ac046a30a311aaa
andreiutu111 <noroc_andrei@yahoo.com>
2021-03-05 00:40:46 +0200

    games_buttons

--- assets/js/firstdemogame.js
+++ assets/js/firstdemogame.js
@@ -1,8 +1,3 @@
 function haasg() {
-	var x = document.getElementById('buttons');
-	var y = document.getElementsByTagName('chosengame');
-
 	window.alert("sometext");
-
-	x.style.visibility = 'hidden';
 }
