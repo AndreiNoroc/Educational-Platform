391488b2b32bd0171244daa5ce274567538080d5
BasarabaRazvan <razvan_basaraba@yahoo.ro>
2021-07-19 23:05:30 +0300

    adaugare la mofal partea de inregistrare

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,11 +1,15 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var loginModal = document.getElementById("loginModal");
+var registerModal = document.getElementById("registerModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("buttonLogin");
+var registration = document.getElementById("new_account");
+var login = document.getElementById("exist_account");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanLogin = document.getElementsByClassName("close")[0];
+var spanRegister = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
@@ -15,24 +19,34 @@ btn.onclick = function() {
     document.getElementById('team').classList.add("blurr");
     document.getElementById('contact').classList.add("blurr");
     document.getElementById('footer').classList.add("blurr");
-    modal.style.display = "block";
+    loginModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+spanLogin.onclick = function() {
     document.getElementById('hero').classList.remove("blurr");
     document.getElementById('tabs').classList.remove("blurr");
     document.getElementById('services').classList.remove("blurr");
     document.getElementById('team').classList.remove("blurr");
     document.getElementById('contact').classList.remove("blurr");
     document.getElementById('footer').classList.remove("blurr");
-    modal.style.display = "none";
+    loginModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+spanRegister.onclick = function() {
+    document.getElementById('hero').classList.remove("blurr");
+    document.getElementById('tabs').classList.remove("blurr");
+    document.getElementById('services').classList.remove("blurr");
+    document.getElementById('team').classList.remove("blurr");
+    document.getElementById('contact').classList.remove("blurr");
+    document.getElementById('footer').classList.remove("blurr");
+    registerModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the loginModal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
         document.getElementById('hero').classList.remove("blurr");
         document.getElementById('tabs').classList.remove("blurr");
         document.getElementById('services').classList.remove("blurr");
@@ -40,4 +54,24 @@ window.onclick = function(event) {
         document.getElementById('contact').classList.remove("blurr");
         document.getElementById('footer').classList.remove("blurr");
     }
+
+    if (event.target == registerModal) {
+        registerModal.style.display = "none";
+        document.getElementById('hero').classList.remove("blurr");
+        document.getElementById('tabs').classList.remove("blurr");
+        document.getElementById('services').classList.remove("blurr");
+        document.getElementById('team').classList.remove("blurr");
+        document.getElementById('contact').classList.remove("blurr");
+        document.getElementById('footer').classList.remove("blurr");
+    }
+}
+
+registration.onclick = function() {
+    loginModal.style.display = "none";
+    registerModal.style.display = "block";
+}
+
+login.onclick = function() {
+    registerModal.style.display = "none";
+    loginModal.style.display = "block";
 }
