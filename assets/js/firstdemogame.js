b373ac35ee7455d8befaadd0900d2778d31ed68e
andreiutu111 <noroc_andrei@yahoo.com>
2021-07-27 17:35:49 +0300

    Second games and comments
    Finish second game concept and put comments in code

--- assets/js/firstdemogame.js
+++ assets/js/firstdemogame.js
@@ -1,3 +1,4 @@
+// Global variable used for first game 
 var gameInProgress = false;
 
 var focus = 0;
@@ -5,11 +6,14 @@ var connections = 0;
 var happines = 0;
 var money = 0;
 
+// Using a map to make a connection between many levels
 let valueElements = new Map();
 
+// All elemnts used in first game
 var listElements = ["barsprog", "initdiv", "initbutt", "exitbutt", "div0vh1", "div0vh2", "div0vh3", 
 "div1vh1", "div2vh1", "div3vh1", "gp1", "gp2", "gp3"];
 
+// Show the first steps in starting the game
 function choosefirstgame() {
 	if (gameInProgress == false) {
 		gameInProgress = true;
@@ -32,6 +36,8 @@ function choosefirstgame() {
 	}
 }
 
+// This function inititates the global variables and create the connections between levels
+// It also show the new interface for game 
 function startgame() {
 	if (document.getElementById("gpa").value === "4-5") {
 		if (document.getElementById("act").value === "Many") {
@@ -105,6 +111,7 @@ function startgame() {
 	valueElements.set("gp3", [1, -5, 3, 100, ["div1vh1"], ["gp2", "gp3"], ["div3vh1"], []]);
 }
 
+// Return to the beginning phase
 function exitfirstgame() {
 	gameInProgress = false;
 
@@ -122,6 +129,7 @@ function exitfirstgame() {
 	document.getElementById("introtext").style.display = 'table-cell';
 }
 
+// This function make the transition from a level to another and make the necessary changes
 function exposeElements(clickedId) {
 	var elemVals = valueElements.get(clickedId);
 
@@ -156,6 +164,7 @@ function exposeElements(clickedId) {
 	}	
 }
 
+// Choose the second game and show the intro of it
 function choosesecondgame() {
 	if (gameInProgress == false) {
 		gameInProgress = true;
@@ -175,6 +184,7 @@ function choosesecondgame() {
 	}
 }
 
+// Start the second game
 function startsecondgame() {
 	document.getElementById("explainsecgame").style.display = 'none';
 	document.getElementById("initbuttsecond").style.display = 'none';
@@ -183,6 +193,7 @@ function startsecondgame() {
 	document.getElementById("citypuzzle").style.display = 'inline-block';
 }
 
+// Let you to drag and drop elements from inventory to table
 function drag(ev) {
     ev.dataTransfer.setData("tablepuzzlepieces", ev.target.id);
 }
@@ -197,6 +208,40 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
+// Find the coordinates of the given element in table
+function findLoc(loc) {
+	for (var i = 1 ; i <= 20 ; i++) {
+		var nm = "div" + i;
+		
+		if (document.getElementById(nm).innerHTML.trim().length != 0) {
+			var moveElem = document.getElementById(nm).getElementsByTagName("*");
+
+			if (loc == moveElem[0].id) {
+				var row = 0;
+				var column = 0;
+
+				if (1 <= i && i <= 5) {
+					row = 1;
+					column = i;
+				} else if (6 <= i && i <= 10) {
+					row = 2;
+					column = i - Math.floor(i / 5) * 5;
+				} else if (11 <= i && i <= 15) {
+					row = 3;
+					column = i - Math.floor(i / 5) * 5;
+				} else if (16 <= i && i <= 20) {
+					row = 4;
+					column = i - Math.floor(i / 5) * 5;
+				}
+
+				return [row, column];
+			}
+		}
+	}
+}
+
+// Check which button was clicked
+// It will show you the score and clear the table
 function exitsecondgame(ev) {
 	if (ev == "finishbuttonsecond") {
 		for (var i = 1 ; i <= 20 ; i++) {
@@ -208,8 +253,18 @@ function exitsecondgame(ev) {
 			}
 		}
 
+		var score = 0;
+
+		var schoolCoord = findLoc("dragschool");
+		var hosptialCoord = findLoc("draghospital");
 		
-		alert("Your score is: ");	
+		var dist = Math.sqrt((schoolCoord[0] - hosptialCoord[0]) * (schoolCoord[0] - hosptialCoord[0]) + (schoolCoord[1] - hosptialCoord[1]) * (schoolCoord[1] - hosptialCoord[1]));
+
+		if (dist < 7) {
+			score = score + 7 - dist;
+		}
+
+		alert("Your score is: " + score);	
 	}
 
 	gameInProgress = false;
